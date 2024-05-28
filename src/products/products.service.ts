44436c2f830aec4f7ed2ce49494a142be9062a77
Franco <francomurabito@outlook.com>
2024-05-28 20:43:56 -0300

    change exception for RpcException

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,13 +1,9 @@
-import {
-  Injectable,
-  Logger,
-  NotFoundException,
-  OnModuleInit,
-} from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from 'src/common/dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -50,8 +46,8 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
 
     if (!product) {
-      this.logger.error(`Product with id: ${id} not found`);
-      throw new NotFoundException(`Product with id: ${id} not found`);
+      // this.logger.error(`Product with id: ${id} not found`);
+      throw new RpcException(`Product with id: ${id} not found`);
     }
 
     return product;
