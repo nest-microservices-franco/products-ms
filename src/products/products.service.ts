e73e5ebdceed7967800699620c3bed446988c18b
Franco <francomurabito@outlook.com>
2024-05-28 21:26:03 -0300

    change RpcException string for object

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
@@ -47,7 +47,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
     if (!product) {
       // this.logger.error(`Product with id: ${id} not found`);
-      throw new RpcException(`Product with id: ${id} not found`);
+      throw new RpcException({
+        status: HttpStatus.NOT_FOUND,
+        message: `Product with id: ${id} not found`,
+      });
     }
 
     return product;
